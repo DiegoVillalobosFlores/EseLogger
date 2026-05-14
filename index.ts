f3fb4010c6b980c9d89dd61bd70c821173b428e1
Diego Villalobos <diegovillalobos397@gmail.com>
2026-05-15 00:12:58 +0200

    Refactor index.ts to add more console methods

--- index.ts
+++ index.ts
@@ -1,3 +1,15 @@
-export default function EsseLogger(...args: Parameters<typeof console.log>) {
-  return console.log(`[${new Date().toISOString()}]: `, ...args);
+import console, { log, info, warn, error } from "node:console";
+
+export default function EseLogger() {
+  return {
+    ...console,
+    log: (args: Parameters<typeof log>) =>
+      log(`[${new Date().toISOString()}]: `, ...args),
+    info: (args: Parameters<typeof info>) =>
+      info(`[${new Date().toISOString()}]: `, ...args),
+    warn: (args: Parameters<typeof warn>) =>
+      warn(`[${new Date().toISOString()}]: `, ...args),
+    error: (args: Parameters<typeof error>) =>
+      error(`[${new Date().toISOString()}]: `, ...args),
+  };
 }
