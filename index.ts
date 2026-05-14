7f8bcce9d3397377565f42a989a257b7ec35d359
Diego Villalobos <diegovillalobos397@gmail.com>
2026-05-15 00:17:36 +0200

    Fix incorrect args array spread in index.ts

--- index.ts
+++ index.ts
@@ -3,13 +3,13 @@ import console, { log, info, warn, error } from "node:console";
 export default function EseLogger() {
   return {
     ...console,
-    log: (args: Parameters<typeof log>) =>
+    log: (...args: Parameters<typeof log>) =>
       log(`[${new Date().toISOString()}]: `, ...args),
-    info: (args: Parameters<typeof info>) =>
+    info: (...args: Parameters<typeof info>) =>
       info(`[${new Date().toISOString()}]: `, ...args),
-    warn: (args: Parameters<typeof warn>) =>
+    warn: (...args: Parameters<typeof warn>) =>
       warn(`[${new Date().toISOString()}]: `, ...args),
-    error: (args: Parameters<typeof error>) =>
+    error: (...args: Parameters<typeof error>) =>
       error(`[${new Date().toISOString()}]: `, ...args),
   };
 }
