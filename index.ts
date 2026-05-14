06fcbc0325c168d3bb51e46596ceef0089411f13
Diego Villalobos <diegovillalobos397@gmail.com>
2026-05-15 00:00:48 +0200

    Update index.ts to remove env based return

--- index.ts
+++ index.ts
@@ -1,6 +1,3 @@
 export default function EsseLogger(...args: Parameters<typeof console.log>) {
-  if (process.env.LOGGER === "custom") {
-    return console.log(`[${new Date().toISOString()}]: `, ...args);
-  }
-  return console.log(...args);
+  return console.log(`[${new Date().toISOString()}]: `, ...args);
 }
